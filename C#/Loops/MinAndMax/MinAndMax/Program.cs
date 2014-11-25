768ae1bf38a7ce3fc24f6b3060233c74f012b2a0
hayleybarnes <hayley.barnes@tombola.com>
2014-11-25 15:57:56 +0000

    min and max task done
    Finished the first loop question for largest and smallest number the
    user has inputted.

---
+++ C#/Loops/MinAndMax/MinAndMax/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinAndMax
+{
+    class Program
+    {
+        static void Main()
+     {
+         int currentMaxValue = Int32.MinValue;
+         int currentMinValue = Int32.MaxValue;
+         for (int i = 0; i < 10; i++)
+         {
+             Console.WriteLine("Your number is:");
+             string userNumberText = Console.ReadLine();
+             int userNumber = int.Parse(userNumberText);
+
+             if (userNumber > currentMaxValue)
+             {
+                 currentMaxValue = userNumber;
+             }
+
+             if (userNumber < currentMinValue)
+             {
+                 currentMinValue = userNumber;
+             }
+         }
+
+             Console.WriteLine("Large Number: {0} Smallest Number: {1}", currentMaxValue, currentMinValue);
+
+             Console.ReadKey();
+         
+        }
+    }
+}
+
