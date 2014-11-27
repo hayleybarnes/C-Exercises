1d9dac7a285e2be75ac92f604cb9e57ae5bb28a8
hayleybarnes <hayley.barnes@tombola.com>
2014-11-27 10:00:25 +0000

    fibonnaci sequence

---
+++ C#/Loops/Fibonnaci/Fibonnaci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonnaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0;
+            int number2 = 1;
+            int sumNumber = 1;
+            Console.WriteLine("Press enter for the first 100 number in 'Fibonnaci Number Sequence':");
+            Console.ReadKey();
+            while (number1 < 100)
+            {
+                sumNumber = number1 + number2;
+                number1 = number2;
+                number2 = sumNumber;
+                Console.WriteLine(number2); 
+            }
+            Console.ReadLine();
+            Console.WriteLine("Press any key to quit the program");
+            Console.ReadLine();
+        }
+    }
+}
