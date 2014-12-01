081bcd54663ef00329742cb83c026a601199fbcf
hayleybarnes <hayley.barnes@tombola.com>
2014-12-01 16:09:03 +0000

    string output task now added to basic data types
    New task has now been added.

---
+++ C#/Basic Data Types/StringOutput/StringOutput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please answer some questions about yourself");
+            Console.WriteLine();
+
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("What is your gender?");
+            String gender = Console.ReadLine(); 
+   
+            Console.WriteLine("Hello {0}, your age is {1}, and gender is {2}", name, age, gender);
+            Console.ReadLine();
+        }
+    }
+}
