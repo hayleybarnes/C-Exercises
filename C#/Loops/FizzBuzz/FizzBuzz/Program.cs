fad7cd9c404b4831327e07b3142803a3bab3adc0
hayleybarnes <hayley.barnes@tombola.com>
2014-12-01 15:16:29 +0000

    fizz buzz task
    finished fizz buzz task

---
+++ C#/Loops/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers 1-100");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizzOutput = i % 3 == 0;
+                bool buzzOutput = i % 5 == 0;
+                if (fizzOutput && buzzOutput)
+                    Console.WriteLine("Fizz Buzz");
+                else if (fizzOutput)
+                    Console.WriteLine("Fizz");
+                else if (buzzOutput)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
