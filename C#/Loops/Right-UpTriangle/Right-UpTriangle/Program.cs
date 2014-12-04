592b4789bcb2360754d2837ed26d29e41fa18599
hayleybarnes <hayley.barnes@tombola.com>
2014-12-04 12:55:22 +0000

    Right sided up triangle
    completed

---
+++ C#/Loops/Right-UpTriangle/Right-UpTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Right_UpTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pascal Triangle:");
+            Console.ReadKey();
+
+            int sizeOfTriangle = 12;
+
+            for (int i = 1; i <= sizeOfTriangle; i++)
+            {
+                for (int j = 1; j <= sizeOfTriangle - i; j++)
+                    Console.Write(" ");
+                for (int x = 1; x <= i; x++)
+                    Console.Write(" *");
+                    Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
