31fd4068482db8bacd943ad7d39d8b3c4e040a33
hayleybarnes <hayley.barnes@tombola.com>
2014-12-04 09:42:48 +0000

    isosceles triangle
    task completed to output a triangle

---
+++ C#/Loops/IsoscelesTriangle/IsoscelesTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoscelesTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Click to see your right isosceles triangle");
+            Console.ReadKey();
+            int i;
+            int j;
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
